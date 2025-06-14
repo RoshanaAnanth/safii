a15b209b1a989b05164e4e5f71e54e5872c23a37
RoshanaAnanth <77053162+RoshanaAnanth@users.noreply.github.com>
2025-06-14 15:55:18 +0530

    Updated App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,27 @@ import React from 'react';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
 
 const App: React.FC = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data, error } = await supabase.auth.getUser();
+      if (data?.user) {
+        setUser(data.user);
+        console.log('Logged in user:', data.user);
+      } else {
+        console.log('No user found');
+      }
+    };
+
+    fetchUser();
+  }, []);
+  
   return (
     <div className="app">
+      {user ? (<h1>Welcome, {user.name}</h1>) : (
+      
+      )}
       <LoginScreen />
     </div>
   );
